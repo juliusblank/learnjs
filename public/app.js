91cb6c9efb6f59aa404f902ec283ddcd3deb86a3
julius.blank <julius.blank@zeiss.com>
2016-10-10 21:03:03 -0500

    add showView function

--- public/app.js
+++ public/app.js
@@ -1,3 +1,8 @@
 // create a namespace for our app so it won't collide with global (e.g. browser) variables
 'use strict';
 var learnjs = {};
+
+learnjs.showView = function(hash) {
+  var problemView = $('<div class="problem-view">').text('Coming sometime soon...');
+  $('.view-container').empty().append(problemView);
+}
