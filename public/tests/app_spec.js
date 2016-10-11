a739a3743e3243edb1e859561351c99b262654a3
julius.blank <julius.blank@zeiss.com>
2016-10-11 11:03:03 -0500

    add listener for hash changes

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,5 +1,11 @@
 describe('LearnJS', function() {
-  
+
+  it('invokes the router when loaded', function()  {
+    spyOn(learnjs, 'showView');
+    learnjs.appOnReady();
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+
   it('can show a problem view', function() {
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
